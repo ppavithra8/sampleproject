af8dd53769e9041e83c41d8d34bd09d0be4add4f
Great Joycs <pavigreatjoycs@gmail.com>
2023-03-14 12:14:55 +0530

    Commit from pavigreatjoycs

--- com/luv2code/pkg/Calculator.java
+++ com/luv2code/pkg/Calculator.java
@@ -1,9 +1,11 @@
 public class Calculator{
   public static int add(int a, int b){
+	System.out.println("addition of 2 numbers");
     return a + b;
   }
   
   public static int sub(int a, int b){
+	  System.out.println("subtraction of 2 numbers");
     return a - b;
   }
 }
